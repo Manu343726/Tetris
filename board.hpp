b1222610090d7eab309572f6470b5a8a8022cd89
Manu343726 <Manu343726@gmail.com>
2014-09-25 13:27:07 +0200

    Pulpillo

---
+++ board.hpp
@@ -0,0 +1,17 @@
+/* 
+ * 
+ * Created on 25 de septiembre de 2014, 12:56
+ */
+
+#ifndef BOARD_HPP
+#define	BOARD_HPP
+
+class piece
+{
+private:
+    
+    std::vector<std::size_t> _parts;
+};
+
+#endif	/* BOARD_HPP */
+
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Manuel
+ *
+ * Created on 25 de septiembre de 2014, 12:03
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
